0c6fc275c6e0b9c4f530650e6619d25ff733cccf
jeffreywangcf <jeffreywangcf@gmail.com>
2023-05-03 21:11:47 +0000

    deployed

--- project/MusicVisualizer/MusicVisualizer/settings.py
+++ project/MusicVisualizer/MusicVisualizer/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@r1w!%8+noku8!554m-a0h0sj-@%hr9#7&28^wr2-=aar8$*o5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/mv/static/'
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR,"static")
 ]
